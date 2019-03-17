604b2694d7448d431349975baf0fb5b5a1fddce7
IgorMakhon <igor.mahon.blr@gmail.com>
2019-03-17 21:57:50 +0300

    few tasks about arrays

---
+++ src/addTwoNumber.java
@@ -0,0 +1,16 @@
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+/*pub1ic class addTwoNumber{
+    public static void main(String[] args) {
+        String[] text;
+        text = new String[2];
+        text[0] = "lf,f";
+        text[1] = "fe;e";
+
+        System.out.println(text[0]);
+
+
+    }
+}
+*/
---
+++ src/arrayString.java
@@ -0,0 +1,6 @@
+public class arrayString {
+    public static void main(String[] args) {
+        String arr[] = {"no", "more", "code"};
+        System.out.println(arr[]);
+    }
+}
